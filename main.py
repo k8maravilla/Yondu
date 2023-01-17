5dffe71159dda2f5d48d0d0bd229677e1912f12e
Kaleb Maravilla <=>
2023-01-16 20:51:30 -0700

    changed shore unit variable name commented debug logging

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ if total_units <= (3 * reavers):
 
 peter_quill_crew = 2
 
-night_crew_units = (reavers-peter_quill_crew) * 3
-print("night_crew_units:", night_crew_units)
+shore_leave = (reavers-peter_quill_crew) * 3
+print("shore leave:", shore_leave)
 
-remaining_units = (total_units-night_crew_units)
-print("remaining_units:", remaining_units)
+remaining_units = (total_units - shore_leave)
+#print("remaining_units:", remaining_units)
 
 #step 2
 yondu_cash = remaining_units * .13
@@ -31,7 +31,7 @@ yondus = int(yondu_cash // 1)
 print("Yondu's share:", yondus)
 
 remaining_units = remaining_units-yondus
-print("remaining_units:", remaining_units)
+#print("remaining_units:", remaining_units)
 
 #step 3
 peter_quill_cash = remaining_units * .11
@@ -39,14 +39,14 @@ peter = int(peter_quill_cash // 1)
 print("Peter's share:", peter)
 
 remaining_units = remaining_units-peter
-print("remaining_units:", remaining_units)
+#print("remaining_units:", remaining_units)
 
 #step 4
 crew_pay_cash= (remaining_units / reavers)
-print("crew_pay_cash", crew_pay_cash)
+#print("crew_pay_cash", crew_pay_cash)
 
 crew_pay_int = int(crew_pay_cash // 1)
-print("Crew:", crew_pay_int)
+print("each crew:", crew_pay_int)
 
 rbf= remaining_units % reavers
 print("RBF:", rbf)
